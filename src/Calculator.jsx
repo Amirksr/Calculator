984781143e9fda7dc3e3f196ecbf1ac1b4afcf75
amir kasraeian <amirkasraeian611@gmail.com>
2024-11-06 11:40:19 +0330

    Implementation of clear and c buttons

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -8,12 +8,18 @@ function Calculator(){
         if(input === "." && result.includes("."))return
         setResult(result + input);
     }
+    const clearBtn = () =>{
+        setResult("");
+    }
+    const backSpaceBtn = () =>{
+        setResult(result.slice(0, -1))
+    }
     return(
         <div className="container">
             <div className="screen">{result}</div>
             <div className="buttons">
-                <button className='color twocol'>Clear</button>
-                <button className='color'>C</button>
+                <button onClick={clearBtn} className='color twocol'>Clear</button>
+                <button onClick={backSpaceBtn} className='color'>C</button>
                 <button onClick={clickHandler} className='color'>÷</button>
                 <button onClick={clickHandler}>7</button>
                 <button onClick={clickHandler}>8</button>
